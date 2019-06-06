ad85c05ef9be50ba796c433e59a6980332f0d664
diegomendezp <diegomen15@gmail.com>
2019-06-06 14:17:13 +0200

    Meta component fixed 🚧

--- templates/components/Meta.js
+++ templates/components/Meta.js
@@ -2,27 +2,28 @@ import Head from "next/head";
 
 const Meta = () => {
   return (
-  <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta charSet="utf-8" />
-      <title>Tailor</title>
-    </Head>
-    <style jsx global>{`
-      @font-face {
-        
-      }
-      
-      body {
-        margin: 0;
-        padding: 0;
-        max-width: 100%;
-      }
+    <React.Fragment>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+        <title>Tailor</title>
+      </Head>
+      <style jsx global>{`
+        @font-face {
+        }
 
-      .* {
-        box-sizing: border-box;
-      }
-    `}</style>
-  )
-}
+        body {
+          margin: 0;
+          padding: 0;
+          max-width: 100%;
+        }
+
+        .* {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </React.Fragment>
+  );
+};
 
 export default Meta;
