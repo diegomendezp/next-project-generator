6136f47cc88fbc22c9f8f5409156cfa47c287310
diegomendezp <diegomen15@gmail.com>
2019-11-18 20:00:40 +0100

    Custom app added

---
+++ templates/pages/_app.js
@@ -0,0 +1,19 @@
+import React from "react";
+import App, { Container } from "next/app";
+import { appWithTranslation, i18n } from "../i18n";
+
+class MyApp extends App {
+  render() {
+    i18n.language ? (i18n.language = i18n.language) : "es";
+    const { Component, pageProps } = this.props;
+    return (
+     
+        <Container>
+          <Component {...pageProps} />
+        </Container>
+  
+    );
+  }
+}
+
+export default appWithTranslation(MyApp);
